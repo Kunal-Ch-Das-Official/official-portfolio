bb120a8a15374f87d44ed4da46e94d0ed98b1b80
Kunal-Ch-Das-Official <kunalchandradasofficial@gmail.com>
2024-11-15 20:23:52 +0530

    Create 404 page

--- src/routes/not-found-route/NotFoundRouter.tsx
+++ src/routes/not-found-route/NotFoundRouter.tsx
@@ -1,5 +1,33 @@
-const NotFoundRouter = () => {
-  return <div>NotFoundRouter</div>;
+import React from "react";
+import style from "./notFound.module.css";
+import { Link } from "react-router-dom";
+const NotFoundRouter: React.FC = () => {
+  return (
+    <section className={style.page404}>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 ">
+            <div className="col-sm-10 col-sm-offset-1  text-center">
+              <h5 className="text-center text-9xl text-gray-600 font-bold">
+                404
+              </h5>
+              <div className={style.fourZerofourBg}></div>
+
+              <div className={style.contantBox404}>
+                <h3 className={style.h2}>Look like you're lost</h3>
+
+                <p>the page you are looking for not avaible!</p>
+
+                <Link to={"/admin-console"} className={style.link404}>
+                  Go to Home
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default NotFoundRouter;
