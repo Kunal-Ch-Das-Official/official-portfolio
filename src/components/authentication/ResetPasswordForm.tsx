26eb1638c0146b49ad37cab18bf9b10b8fd6a074
Kunal-Ch-Das-Official <kunalchandradasofficial@gmail.com>
2024-11-15 20:04:42 +0530

    fix css of the forms

--- src/components/authentication/ResetPasswordForm.tsx
+++ src/components/authentication/ResetPasswordForm.tsx
@@ -99,7 +99,7 @@ const ResetPasswordForm: React.FC = () => {
         buttonColor={loginResponse.buttonColor}
       />
       <section>
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="flex flex-col items-center justify-center px-0 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-2 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold text-gray-500 md:text-2xl">
